4c0af5b60ab7f43493859d9862db457714302712
Royal Dwip <130228344+TechnicalDwip@users.noreply.github.com>
2023-07-04 09:52:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 13708534
 API_HASH     = "51b384fee3c86840ee2ba7938f0beff4"
-BOT_TOKEN    = "6083504683:AAEZZAePsjvcKGWOAMNTlaveNl7DD5KZLyo"
+BOT_TOKEN    = "6140076272:AAFZ8aws2jwY-tg1EA8J6sWY1xmqQ7FT33Q"
 SESSION      = "BQEVDDgAqAeqpwh6ISGOHhApFeAW2aOQ1fgvqNm5kH5dR2DFH3AGfjSAG6gtsq4Mz2Ra6uekSou5X1Breuqjs-DTnHtHNfRVDj3RB86iRFlIF1mA7R4kHDvGF-qVFGMbUjePCSFarxFZch-AHNN4SspYH_i3KHN5h_juhSOSlT5O2iVGIq2CNp22_aPxWkifsBrQnUuyJ3tKTy7_HQ734ICQ5RATZtWtMdO0kCUbY6TyhgYJ0L8jm7AnsqnEmBdM2UkF4fuuasDrOmDocrm39vbITKNgjnucAODx2AtY8bSD5qNDx-BH_vIORzuGF72wqzU19kDzlpyb_-Uqx483blPHyKfOQgAAAAFhSwkGAA"
-DATABASE_URI = "mongodb+srv://MdiskDb:MdiskDb@cluster0.lmirmtn.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://worldofmoviesorg:worldofmoviesorg@cluster0.1ewydrj.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001976813589
 ADMIN        = 5196437706
